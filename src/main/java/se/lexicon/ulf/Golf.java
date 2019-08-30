ff66bc244d8d288c8478a78a553ae9062cd853a7
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-08-30 09:23:22 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/ulf/Golf.java
@@ -0,0 +1,26 @@
+package se.lexicon.ulf;
+
+public class Golf {
+
+    /**
+     * Converts angle in degrees to angle in radians
+     * (Pi / 180) * angle = angle in radians
+     * @param angle in degrees
+     * @return angle in radians
+     */
+    public static double angleToRadius(double angle)
+    {
+        return 0.0;
+    }
+
+    /**
+     * Only based on Velocity/Gravity/Angle
+     * GRAVITY = 9.8
+     * Velocity^2 / GRAVITY * Sin( 2 * Angle in radians ) = Distance
+     * @return Distance traveled by the ball
+     */
+    public static double ballDistanceTraveled(double velocity, double angle)
+    {
+        return 0.0;
+    }
+}
---
+++ src/test/java/se/lexicon/ulf/AppTest.java
@@ -0,0 +1,45 @@
+package se.lexicon.ulf;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Example: 45 Degrees ≈ 0.785398 Radians
+     */
+    @Test
+    public void ConvertAngleDegreesToAngleRadians_test_45()
+    {
+        //Arrange
+        double angle = 45;
+
+        //Act
+        double result = Golf.angleToRadius(angle);
+
+        //Assert
+        assertEquals(0.785398, result, 4);
+    }
+
+    /**
+     * Example: At 45 Degrees and 56 m/s, the ball should travel 320 meters.
+     */
+    @Test
+    public void ballDistanceTraveled_test_320_meters()
+    {
+        //Arrange
+        double angle = 45;
+        double velocity = 56;
+
+        //Act
+        double result = Golf.ballDistanceTraveled( velocity, angle );
+
+        //Assert
+        assertEquals(320.0, result, 4);
+    }
+}
